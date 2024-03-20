8d82096e37617474323d77c04e438620e3618831
TheRedSwaRM <rsbalatibat@up.edu.ph>
2024-03-20 16:45:30 +0800

    feat: display individual FHIR data from server based on patient ID

--- onFHIR/src/pages/Home.tsx
+++ onFHIR/src/pages/Home.tsx
@@ -1,9 +1,42 @@
-import { Center, Title } from "@mantine/core";
+import { useFhirRead } from "@bonfhir/query/r4b";
+import { FhirQueryLoader, FhirValue } from "@bonfhir/react/r4b";
+import { Group, Paper, Stack, Text } from "@mantine/core";
 
 export default function Home() {
+  const patientQuery = useFhirRead(
+    "Patient",
+    "6bc3dd32-a3a8-4bb8-8158-c83fc2780fd2",
+  );
+  
   return (
-    <Center h="100vh">
-      <Title>BonFHIR + Mantine + Vite = 🔥</Title>
-    </Center>
+    <Paper p="xl"> {/*Mantine Component, to learn more, read Mantine documentation*/}
+      <Paper shadow="sm" p="xl">
+        <FhirQueryLoader query={patientQuery}>
+          {(patient) => (
+            <Stack gap="xs">
+              <Text size="xl">
+                <FhirValue type="HumanName" value={patient.name} />
+              </Text>
+              <Group>
+                <Text fw={600}>Birthday: </Text>
+                <FhirValue
+                  type="date"
+                  value={patient.birthDate}
+                  options={{ dateStyle: "full" }}
+                />
+              </Group>
+              <Group>
+                <Text fw={600}>Address: </Text>
+                <FhirValue type="Address" value={patient.address} />
+              </Group>
+              <Group>
+                <Text fw={600}>Contact: </Text>
+                <FhirValue type="ContactPoint" value={patient.telecom} />
+              </Group>
+            </Stack>
+          )}
+        </FhirQueryLoader>
+      </Paper>
+    </Paper>
   );
 }
